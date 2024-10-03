9805b19e490c717273368200da987ac434e55b2e
Phu Hoang <phoang5@vols.utk.edu>
2024-10-03 01:19:54 -0400

    Issue #2: The second type of framewok is a native iOS app

---
+++ projects/framework-research/ios-app/ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Phu Hoang on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
