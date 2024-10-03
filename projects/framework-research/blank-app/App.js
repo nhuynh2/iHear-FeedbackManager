c18ff4682979e4626effece77cc1e25ba99e1645
Phu Hoang <phoang5@vols.utk.edu>
2024-10-03 01:45:27 -0400

    Issue #2: The third type of framework using Development Build, but with blank mode to simplify some modules.

---
+++ projects/framework-research/blank-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is just a blank app</Text>
+      <Text>No framework, no pre-installed modules</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
