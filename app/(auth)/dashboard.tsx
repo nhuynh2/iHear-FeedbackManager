585a4c0cf68bc8c4cec772aba1ff7d25ca293c65
Phu Hoang <phoang5@vols.utk.edu>
2024-11-08 02:21:49 -0500

    Revert from corruption #90

--- app/(auth)/dashboard.tsx
+++ app/(auth)/dashboard.tsx
@@ -14,18 +14,7 @@ import {
 import { firestore } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
-import AddTicketButton from "../../components/AddTicketButton.tsx";
-        
-import { useRouter } from 'expo-router';
-import { ROUTES } from "../../components/navigation/routes";
 import { useIsFocused } from "@react-navigation/native";
-        
-import Animated, {
-    useAnimatedRef,
-    useAnimatedStyle,
-    useScrollViewOffset,
-    withTiming,
-    } from 'react-native-reanimated';
 
 const HEIGHT = Dimensions.get("screen").height;
 const WIDTH = Dimensions.get("screen").width;
@@ -94,16 +83,8 @@ export default function Dashboard() {
     getData();
   };
 
-  const router = useRouter(); // Move useRouter hook here
-    const scrollRef = useAnimatedRef<Animated.ScrollView>();
-    const scrollHandler = useScrollViewOffset(scrollRef);
-    const buttonStyle = useAnimatedStyle(() => {
-        return {
-            opacity: scrollHandler.value < 60 ? withTiming(1) : withTiming(0),
-            }
-        });
   const onPressHandle = () => {
-      router.push(ROUTES.TICKET_DETAILS); // Navigate to the Ticket Details page
+    alert("Clicked!");
   };
 
   return (
@@ -114,7 +95,6 @@ export default function Dashboard() {
         <ActivityIndicator size="large" />
       ) : (
         <FlatList
-          ref={scrollRef}
           scrollEnabled={true}
           scrollToOverflowEnabled={true}
           removeClippedSubviews={true}
@@ -154,13 +134,10 @@ export default function Dashboard() {
           refreshing={loading} // Controls the loading indicator for pull-to-refresh
           onRefresh={handleRefresh} // Pull-to-refresh handler
         />
-        <Animated.View style ={[buttonStyle, { position: 'absolute', right: 10, bottom: 10 } ]}>
-          <AddTicketButton />
-        </Animated.View>
       )}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
